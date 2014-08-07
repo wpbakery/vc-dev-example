e8bb4d7ea4058a7d48d1f9f0a89cf8c7b74b6a07
time2type <time2type@gmail.com>
2014-08-07 15:14:24 +0300

    1. Load on init 2. Console log small fix

--- assets/front_enqueue_js.js
+++ assets/front_enqueue_js.js
@@ -1,10 +1,10 @@
-console.log('front_enqueue_js.js is loaded');
+console && console.log('front_enqueue_js.js is loaded');
 
 // This name is defined automatically (InlineShortcodeView_you, for Frontend editor only
 window.InlineShortcodeView_test_element = window.InlineShortcodeView.extend({
 	// Render called every time when some of attributes changed.
 	render: function () {
-		console.log('InlineShortcodeView_test_element: render called.');
+        console && console.log('InlineShortcodeView_test_element: render called.');
 		window.InlineShortcodeView_test_element.__super__.render.call(this); // it is recommended to call parent method to avoid new versions problems.
 
 		// There is a place where you can implement logic for rendering / element param changing and all other javascript logic what you can imagine.
@@ -25,7 +25,7 @@ window.InlineShortcodeView_test_element = window.InlineShortcodeView.extend({
 		jQuery('<div>Green background will be visible only in fronteditor mode and css is stored in assets/front_enqueue_iframe_css.css <br/><br/> This json styled info was created "on the fly" from available settings: <br/>' + str + '</div>').appendTo(this.$el);
 	},
 	updated: function () {
-		console.log('InlineShortcodeView_test_element: updated called.');
+        console && console.log('InlineShortcodeView_test_element: updated called.');
 		window.InlineShortcodeView_test_element.__super__.updated.call(this);
 
 		// This is example how you can re-render pieChart Doughnut in frontend editor.
@@ -33,7 +33,7 @@ window.InlineShortcodeView_test_element = window.InlineShortcodeView.extend({
 		window.vc.frame_window.TestElementRender($i);
 	},
 	parentChanged: function () {
-		console.log('InlineShortcodeView_test_element: parentChanged called.');
+        console && console.log('InlineShortcodeView_test_element: parentChanged called.');
 		window.InlineShortcodeView_test_element.__super__.parentChanged.call(this);
 	}
 	// Available other methods too, see in InlineShortcodeView model (file: js_composer/assets/js/frontend_editor/frontend_editor.js
